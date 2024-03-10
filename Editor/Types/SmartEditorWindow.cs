e7579524947394b2e5dd734be093705d37e4537d
Luis Masuelli <luisfmasuelli@gmail.com>
2024-03-10 18:51:15 -0500

    Adding more smart methods and refactoring the life cycle a while to remove the overridable after-hook.

--- Editor/Types/SmartEditorWindow.cs
+++ Editor/Types/SmartEditorWindow.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,9 +9,22 @@ namespace AlephVault.Unity.MenuActions
     {
         /// <summary>
         ///   This is a smart editor class. It auto-adjusts the size.
+        ///   It also provides a way to safely queue actions to do
+        ///   after adjusting (e.g. executing a button).
         /// </summary>
         public abstract class SmartEditorWindow : EditorWindow
         {
+            /// <summary>
+            ///   The close type for when an action is executed.
+            /// </summary>
+            public enum CloseType
+            {
+                NoClose, OnSuccess, Always
+            }
+
+            // The actions to execute.
+            private List<Action> afterAdjustedGUI = new();
+
             /// <summary>
             ///   Runs all the inner GUI logic but wrapping it in a
             ///   Vertical group, and then calculates the size so it
@@ -51,10 +66,82 @@ namespace AlephVault.Unity.MenuActions
             /// </summary>
             protected virtual void OnAdjustedGUI() {}
             
+            // Executes all the queued actions.
+            private void OnAfterAdjustedGUI()
+            {
+                try
+                {
+                    foreach (Action action in afterAdjustedGUI)
+                    {
+                        action();
+                    }
+                }
+                finally
+                {
+                    afterAdjustedGUI = new();
+                }
+            }
+
+            // ReSharper disable Unity.PerformanceAnalysis
             /// <summary>
-            ///   Applies whatever post-render logic is needed.
+            ///   Executes an action smartly. It actually waits
+            ///   for the UI to be completely rendered and the
+            ///   window size to be adjusted and then it runs
+            ///   the specified action.
             /// </summary>
-            protected virtual void OnAfterAdjustedGUI() {}
+            /// <param name="action">The action to execute</param>
+            /// <param name="closeType">Whether to close (and how) or not the window</param>
+            protected void SmartExecute(Action action, CloseType closeType = CloseType.NoClose)
+            {
+                Action finalAction;
+                switch (closeType)
+                {
+                    case CloseType.OnSuccess:
+                        finalAction = () =>
+                        {
+                            action();
+                            Close();
+                        };
+                        break;
+                    case CloseType.Always:
+                        finalAction = () =>
+                        {
+                            try
+                            {
+                                action();
+                            }
+                            finally
+                            {
+                                Close();
+                            }
+                        };
+                        break;
+                    default:
+                        finalAction = action;
+                        break;
+                }
+                afterAdjustedGUI.Add(finalAction);
+            }
+
+            /// <summary>
+            ///   Renders a GUI button that, when clicked, executes
+            ///   an action smartly (see <see cref="SmartExecute"/>
+            ///   for more details).
+            /// </summary>
+            /// <param name="text">The button text</param>
+            /// <param name="action">The action to execute</param>
+            /// <param name="closeType">Whether to close (and how) or not the window</param>
+            /// <returns>Whether the button was pressed or not</returns>
+            protected bool SmartButton(string text, Action action, CloseType closeType = CloseType.NoClose)
+            {
+                if (GUILayout.Button(text))
+                {
+                    SmartExecute(action, closeType);
+                    return true;
+                }
+
+                return false;
+            }
         }
     }
 }
